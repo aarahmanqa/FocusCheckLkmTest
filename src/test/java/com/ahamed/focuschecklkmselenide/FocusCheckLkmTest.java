aa79174ccbd4217c97eada26e268e32719d57fed
aarahman <ahamedabdulrahman@gmail.com>
2025-02-02 01:39:56 +0530

    Optimized the program the final score is 1680

--- src/test/java/com/ahamed/focuschecklkmselenide/FocusCheckLkmTest.java
+++ src/test/java/com/ahamed/focuschecklkmselenide/FocusCheckLkmTest.java
@@ -13,31 +13,26 @@ public class FocusCheckLkmTest {
     @Test
     public void test() {
         Configuration.browserSize = "max";
-        SelenideLogger.addListener("allure", new AllureSelenide());
+        SelenideLogger.addListener("allure", new AllureSelenide().screenshots(true).savePageSource(true));
         Selenide.open("https://focus-check-lkm-001.web.app/");
         WebDriverRunner.getWebDriver().manage().window().maximize();
         Configuration.timeout = 3000;
 
-        $x("//input[@placeholder='Your Name']").setValue("Ahamed");
+        $x("//input[@placeholder='Your Name']").setValue("https://github.com/aarahmanqa");
         $(byText("Start Game")).click();
 
-        while(true) {
-            String numberToBeHit = $x("//li[contains(text(),'HIT ')]/span").getText();
-            System.out.println("Number to be hit: " + numberToBeHit);
-            boolean clickDone = false;
-            int size = $$x("//p[contains(@class,'text-black-900')]").size();
-            for(int i=0; i<size; i++) {
-                String currentNumber = $$x("//p[contains(@class,'text-black-900')]").get(i).getText();
-                System.out.println("Current Number : " + currentNumber);
-                if (currentNumber.equals(numberToBeHit)) {
-                    $$x("//p[contains(@class,'text-black-900')]").get(i).click();
-                    clickDone = true;
-                    break;
-                }
+        while (true) {
+            SelenideElement numberToBeHitElement = $x("//li[contains(text(),'HIT ')]/span");
+            if (!numberToBeHitElement.exists()) {
+                break;
             }
-            if(!clickDone) {
-                throw new RuntimeException("Unable to perform click");
+            String numberToBeHit = numberToBeHitElement.getText();
+            SelenideElement currentNumberElement = $x("//p[contains(@class,'text-black-900')][text()='" + numberToBeHit + "']");
+            if (!currentNumberElement.exists()) {
+                break;
             }
+            currentNumberElement.click();
         }
+        System.out.println($(byText("score :")).getText());
     }
 }
